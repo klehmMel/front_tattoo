9e1449fc3b7d13456f5e42be15435bb1780348c2
klehmMel <klehm.m@aluno.ifsp.edu.br>
2025-04-29 17:27:34 -0300

    parteDeBusca

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,37 @@ document.addEventListener('DOMContentLoaded', function (){
             
         })
     })
+    
+// fechar o dropdown ao clicar fora
+document.addEventListener('click', function(e) {
+    dropdownToggle.forEach((toggle) => {
+        const parent = toggle.closest('.dropdown-item');
+        if (!parent.contains(e.target)){ 
+            parent.classList.remove('active');
+        }
+    })
+    }) 
 })
+
+const searchToggle = document.getElementById('searchToggle');
+const searchOverlay = document.getElementById('searchOverlay');
+const closeSearch = document.getElementById('closeSearch');
+
+searchToggle.addEventListener('click', () => {
+    searchOverlay.style.display = 'flex';
+    setTimeout(() => {
+        searchOverlay.querySelector('.search-input').focus();
+    }, 100);
+});
+
+// fechar o botão 
+closeSearch.addEventListener('click', () => {
+    searchOverlay.style.display = 'none';
+})
+
+// fechar com o ESC
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        searchOverlay.style.display = 'none';
+    }
+} )
